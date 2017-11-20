ecb154c09aa979f74428270f31afeebdaacfde53
Nick-Shmyrev <nr.shmyrev@gmail.com>
2017-11-19 20:01:40 -0500

    Add greeting to a user, and alert to everyone else

--- server/server.js
+++ server/server.js
@@ -14,14 +14,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
+  // emits a message to a single user
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to chat app',
+    createdAt: new Date().getTime()
+  });
+
+  // emits a message to everyone but the user
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   // listen for new messages FROM clients, emit them back TO clients
   socket.on('createMessage', (message) => {
     console.log('Msg from client', message);
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
-      createdAt: Date.now()
+      createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: Date.now()
+    // });
+
   })
 
   socket.on('disconnect', (socket) => {
